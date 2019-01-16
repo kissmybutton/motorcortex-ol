68fd3c69f04c54134ae1abde5a6a1f07a0fab478
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2019-01-16 16:17:16 +0200

    feat: implement custom animate function for ol maps

--- dist/ZoomTo.js
+++ dist/ZoomTo.js
@@ -21,7 +21,7 @@ function _assertThisInitialized(self) { if (self === void 0) { throw new Referen
 var MC = require("@kissmybutton/motorcortex");
 
 var _require = require("ol/easing.js"),
-    linear = _require.linear;
+    inAndOut = _require.inAndOut;
 
 var ZoomTo =
 /*#__PURE__*/
@@ -37,17 +37,26 @@ function (_MC$TimedIncident) {
     _this.previousMs = -100;
     _this.zoomChanged = true;
     _this.onCompleteZoom = _this.onCompleteZoom.bind(_assertThisInitialized(_assertThisInitialized(_this)));
+    _this.init = _this.init.bind(_assertThisInitialized(_assertThisInitialized(_this)));
     return _this;
   }
 
   _createClass(ZoomTo, [{
+    key: "onGetContext",
+    value: function onGetContext() {
+      global.map = this.context.mapRef;
+      this.view = this.context.mapRef.getView();
+      this.init(this.attrs.animatedAttrs.goto);
+    }
+  }, {
     key: "getScratchValue",
     value: function getScratchValue()
     /*mcid, attribute*/
     {
       var _goto = {
         zoom: this.context.mapRef.getView().getZoom(),
-        center: this.context.mapRef.getView().getCenter()
+        center: this.context.mapRef.getView().getCenter(),
+        rotation: this.context.mapRef.getView().getRotation()
       };
       return _goto;
     }
@@ -62,37 +71,77 @@ function (_MC$TimedIncident) {
       this.centerChanged = true;
     }
   }, {
-    key: "onProgress",
-    value: function onProgress(progress, millisecond) {
-      for (var key in this.attrs.animatedAttrs) {
-        var delta = Math.abs(this.previousMs - millisecond);
+    key: "init",
+    value: function init(options) {
+      var initialValue = this.getInitialValue("goto");
+      var center = initialValue.center.slice();
+      var resolution = this.view.getResolutionForZoom(initialValue.zoom);
+      var rotation = initialValue.rotation;
+      this.animation = {
+        anchor: options.anchor,
+        easing: options.easing || inAndOut
+      };
 
-        if (this.zoomChanged && delta > 80 || progress >= 1 || progress <= 0) {
-          var view = this.context.mapRef.getView();
-          var initialValue = this.getInitialValue(key);
-          var initialZoom = initialValue.zoom;
-          var attributesZoom = this.attrs.animatedAttrs.goto.zoom;
-          var calcZoom = initialZoom - attributesZoom;
-          var zoom = initialZoom - calcZoom * progress;
-          var initialCenterX = initialValue.center[0];
-          var attributesCenterX = this.attrs.animatedAttrs.goto.center[0];
-          var calcCenterX = initialCenterX - attributesCenterX;
-          var x = initialCenterX - calcCenterX * progress;
-          var initialCenterY = initialValue.center[1];
-          var attributesCenterY = this.attrs.animatedAttrs.goto.center[1];
-          var calcCenterY = initialCenterY - attributesCenterY;
-          var y = initialCenterY - calcCenterY * progress;
-          view.cancelAnimations();
-          view.animate({
-            center: [x, y],
-            zoom: zoom,
-            duration: delta,
-            easing: linear
-          }, this.onCompleteZoom);
-          this.zoomChanged = false;
-          this.previousMs = millisecond;
-        }
+      if (options.center) {
+        this.animation.sourceCenter = center;
+        this.animation.targetCenter = options.center;
       }
+
+      if (options.zoom !== undefined) {
+        this.animation.sourceResolution = resolution;
+        this.animation.targetResolution = this.view.constrainResolution(this.view.maxResolution_, options.zoom - this.view.minZoom_, 0);
+      } else if (options.resolution) {
+        this.animation.sourceResolution = resolution;
+        this.animation.targetResolution = options.resolution;
+      }
+
+      if (options.rotation !== undefined) {
+        this.animation.sourceRotation = rotation;
+        var delta = (options.rotation - rotation + Math.PI) % (2 * Math.PI) - Math.PI;
+        this.animation.targetRotation = rotation + delta;
+      }
+    }
+  }, {
+    key: "animate",
+    value: function animate(progress) {
+      progress = this.animation.easing(progress);
+
+      if (this.animation.sourceCenter) {
+        var x0 = this.animation.sourceCenter[0];
+        var y0 = this.animation.sourceCenter[1];
+        var x1 = this.animation.targetCenter[0];
+        var y1 = this.animation.targetCenter[1];
+        var x = x0 + progress * (x1 - x0);
+        var y = y0 + progress * (y1 - y0);
+        this.view.setCenter([x, y]);
+      }
+
+      if (this.animation.sourceResolution && this.animation.targetResolution) {
+        var resolution = progress === 1 ? this.animation.targetResolution : this.animation.sourceResolution + progress * (this.animation.targetResolution - this.animation.sourceResolution);
+
+        if (this.animation.anchor) {
+          this.view.setCenter(this.view.calculateCenterZoom(resolution, this.animation.anchor));
+        }
+
+        this.view.setResolution(resolution);
+      }
+
+      if (this.animation.sourceRotation !== undefined && this.animation.targetRotation !== undefined) {
+        var rotation = progress === 1 ? (this.animation.targetRotation + Math.PI) % (2 * Math.PI) - Math.PI : this.animation.sourceRotation + progress * (this.animation.targetRotation - this.animation.sourceRotation);
+
+        if (this.animation.anchor) {
+          this.view.setCenter(this.view.calculateCenterRotate(rotation, this.animation.anchor));
+        }
+
+        this.view.setRotation(rotation);
+      }
+    }
+  }, {
+    key: "onProgress",
+    value: function onProgress(progress
+    /*, millisecond*/
+    ) {
+      this.animate(progress);
     }
   }]);
 
--- src/ZoomTo.js
+++ src/ZoomTo.js
@@ -1,5 +1,5 @@
 const MC = require("@kissmybutton/motorcortex");
-const { linear } = require("ol/easing.js");
+const { inAndOut } = require("ol/easing.js");
 
 class ZoomTo extends MC.TimedIncident {
   constructor(attrs, props) {
@@ -7,11 +7,19 @@ class ZoomTo extends MC.TimedIncident {
     this.previousMs = -100;
     this.zoomChanged = true;
     this.onCompleteZoom = this.onCompleteZoom.bind(this);
+    this.init = this.init.bind(this);
   }
+  onGetContext() {
+    global.map = this.context.mapRef;
+    this.view = this.context.mapRef.getView();
+    this.init(this.attrs.animatedAttrs.goto);
+  }
+
   getScratchValue(/*mcid, attribute*/) {
     const _goto = {
       zoom: this.context.mapRef.getView().getZoom(),
-      center: this.context.mapRef.getView().getCenter()
+      center: this.context.mapRef.getView().getCenter(),
+      rotation: this.context.mapRef.getView().getRotation()
     };
     return _goto;
   }
@@ -22,43 +30,90 @@ class ZoomTo extends MC.TimedIncident {
   onCompleteCenter() {
     this.centerChanged = true;
   }
-  onProgress(progress, millisecond) {
-    for (const key in this.attrs.animatedAttrs) {
-      const delta = Math.abs(this.previousMs - millisecond);
-      if ((this.zoomChanged && delta > 80) || progress >= 1 || progress <= 0) {
-        const view = this.context.mapRef.getView();
 
-        const initialValue = this.getInitialValue(key);
-        const initialZoom = initialValue.zoom;
-        const attributesZoom = this.attrs.animatedAttrs.goto.zoom;
-        const calcZoom = initialZoom - attributesZoom;
-        const zoom = initialZoom - calcZoom * progress;
+  init(options) {
+    const initialValue = this.getInitialValue("goto");
+    const center = initialValue.center.slice();
+    const resolution = this.view.getResolutionForZoom(initialValue.zoom);
+    const rotation = initialValue.rotation;
 
-        const initialCenterX = initialValue.center[0];
-        const attributesCenterX = this.attrs.animatedAttrs.goto.center[0];
-        const calcCenterX = initialCenterX - attributesCenterX;
-        const x = initialCenterX - calcCenterX * progress;
+    this.animation = {
+      anchor: options.anchor,
+      easing: options.easing || inAndOut
+    };
 
-        const initialCenterY = initialValue.center[1];
-        const attributesCenterY = this.attrs.animatedAttrs.goto.center[1];
-        const calcCenterY = initialCenterY - attributesCenterY;
-        const y = initialCenterY - calcCenterY * progress;
-
-        view.cancelAnimations();
-
-        view.animate(
-          {
-            center: [x, y],
-            zoom,
-            duration: delta,
-            easing: linear
-          },
-          this.onCompleteZoom
-        );
-        this.zoomChanged = false;
-        this.previousMs = millisecond;
-      }
+    if (options.center) {
+      this.animation.sourceCenter = center;
+      this.animation.targetCenter = options.center;
     }
+
+    if (options.zoom !== undefined) {
+      this.animation.sourceResolution = resolution;
+      this.animation.targetResolution = this.view.constrainResolution(
+        this.view.maxResolution_,
+        options.zoom - this.view.minZoom_,
+        0
+      );
+    } else if (options.resolution) {
+      this.animation.sourceResolution = resolution;
+      this.animation.targetResolution = options.resolution;
+    }
+
+    if (options.rotation !== undefined) {
+      this.animation.sourceRotation = rotation;
+      const delta =
+        ((options.rotation - rotation + Math.PI) % (2 * Math.PI)) - Math.PI;
+      this.animation.targetRotation = rotation + delta;
+    }
+  }
+
+  animate(progress) {
+    progress = this.animation.easing(progress);
+    if (this.animation.sourceCenter) {
+      const x0 = this.animation.sourceCenter[0];
+      const y0 = this.animation.sourceCenter[1];
+      const x1 = this.animation.targetCenter[0];
+      const y1 = this.animation.targetCenter[1];
+      const x = x0 + progress * (x1 - x0);
+      const y = y0 + progress * (y1 - y0);
+      this.view.setCenter([x, y]);
+    }
+    if (this.animation.sourceResolution && this.animation.targetResolution) {
+      const resolution =
+        progress === 1
+          ? this.animation.targetResolution
+          : this.animation.sourceResolution +
+            progress *
+              (this.animation.targetResolution -
+                this.animation.sourceResolution);
+      if (this.animation.anchor) {
+        this.view.setCenter(
+          this.view.calculateCenterZoom(resolution, this.animation.anchor)
+        );
+      }
+      this.view.setResolution(resolution);
+    }
+    if (
+      this.animation.sourceRotation !== undefined &&
+      this.animation.targetRotation !== undefined
+    ) {
+      const rotation =
+        progress === 1
+          ? ((this.animation.targetRotation + Math.PI) % (2 * Math.PI)) -
+            Math.PI
+          : this.animation.sourceRotation +
+            progress *
+              (this.animation.targetRotation - this.animation.sourceRotation);
+      if (this.animation.anchor) {
+        this.view.setCenter(
+          this.view.calculateCenterRotate(rotation, this.animation.anchor)
+        );
+      }
+      this.view.setRotation(rotation);
+    }
+  }
+  onProgress(progress /*, millisecond*/) {
+    this.animate(progress);
   }
 }
 
