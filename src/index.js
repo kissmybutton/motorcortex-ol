ba8d24155a3a138ca15fbd69a7c0e904d1802c27
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-04-10 18:59:08 +0300

    fix:add version and name at index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Clip from "./MapClip";
 import GoTo from "./GoTo";
 import { fromLonLat } from "ol/proj.js";
+import { name, version } from "../package.json";
 
 export default {
-  npm_name: "@kissmybutton/motorcortex-ol",
+  npm_name: name, // don't touch this
+  version: version, // don't touch this
   incidents: [
     {
       exportable: GoTo,
@@ -16,17 +18,17 @@ export default {
               type: "object",
               props: {
                 zoom: { type: "number", min: 0 },
-                center: { type: "array", items: "number", min: 2, max: 2 }
-              }
-            }
-          }
-        }
-      }
-    }
+                center: { type: "array", items: "number", min: 2, max: 2 },
+              },
+            },
+          },
+        },
+      },
+    },
   ],
   compositeAttributes: { goto: ["center", "zoom"] },
   Clip,
   utils: {
-    fromLonLat
-  }
+    fromLonLat,
+  },
 };
