8a14e85848ebfe70b7c5ff8285474cbc4263cd80
Alexandros Dorodoulis <aldor94@gmail.com>
2022-03-18 12:26:24 +0200

    refactor: cache some function calls

--- src/GoTo.js
+++ src/GoTo.js
@@ -23,26 +23,27 @@ export default class ZoomTo extends Effect {
   }
 
   getScratchValue() {
-    const _goto = {
-      zoom: this.element.entity.getView().getZoom(),
-      center: this.element.entity.getView().getCenter(),
-      rotation: this.element.entity.getView().getRotation(),
+    const view = this.element.entity.getView();
+    return {
+      zoom: view.getZoom(),
+      center: view.getCenter(),
+      rotation: view.getRotation(),
     };
-
-    return _goto;
   }
 
-  onProgress(millisecond /*, millisecond*/) {
+  onProgress(millisecond) {
+    const fraction = this.getFraction(millisecond);
     //this has better effect than mc easings
     /*
     CHANGE MAP CENTER
     */
-    const x0 = this.animation.sourceCenter[0];
-    const y0 = this.animation.sourceCenter[1];
-    const x1 = this.animation.targetCenter[0];
-    const y1 = this.animation.targetCenter[1];
-    const x = x0 + this.getFraction(millisecond) * (x1 - x0);
-    const y = y0 + this.getFraction(millisecond) * (y1 - y0);
+    const animation = this.animation;
+    const x0 = animation.sourceCenter[0];
+    const y0 = animation.sourceCenter[1];
+    const x1 = animation.targetCenter[0];
+    const y1 = animation.targetCenter[1];
+    const x = x0 + fraction * (x1 - x0);
+    const y = y0 + fraction * (y1 - y0);
 
     this.view.setCenter([x, y]);
 
@@ -50,14 +51,13 @@ export default class ZoomTo extends Effect {
     CHANGE MAP RESOLUTION
     */
     const resolution =
-      this.getFraction(millisecond) === 1
-        ? this.animation.targetResolution
-        : this.animation.sourceResolution +
-          this.getFraction(millisecond) *
-            (this.animation.targetResolution - this.animation.sourceResolution);
-    if (this.animation.anchor) {
+      fraction === 1
+        ? animation.targetResolution
+        : animation.sourceResolution +
+          fraction * (animation.targetResolution - animation.sourceResolution);
+    if (animation.anchor) {
       this.view.setCenter(
-        this.view.calculateCenterZoom(resolution, this.animation.anchor)
+        this.view.calculateCenterZoom(resolution, animation.anchor)
       );
     }
     this.view.setResolution(resolution);
@@ -66,14 +66,13 @@ export default class ZoomTo extends Effect {
     CHANGE MAP ROTATION
     */
     const rotation =
-      this.getFraction(millisecond) === 1
-        ? ((this.animation.targetRotation + Math.PI) % (2 * Math.PI)) - Math.PI
-        : this.animation.sourceRotation +
-          this.getFraction(millisecond) *
-            (this.animation.targetRotation - this.animation.sourceRotation);
-    if (this.animation.anchor) {
+      fraction === 1
+        ? ((animation.targetRotation + Math.PI) % (2 * Math.PI)) - Math.PI
+        : animation.sourceRotation +
+          fraction * (animation.targetRotation - animation.sourceRotation);
+    if (animation.anchor) {
       this.view.setCenter(
-        this.view.calculateCenterRotate(rotation, this.animation.anchor)
+        this.view.calculateCenterRotate(rotation, animation.anchor)
       );
     }
     this.view.setRotation(rotation);
